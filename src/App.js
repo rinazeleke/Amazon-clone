76c391748fbd827f7eda714ab3bb6ab711a97e94
rinazeleke <rinazeleke1994@gmail.com>
2024-05-23 05:22:46 -1200

    product component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import CarouselEffect from './Components/Carousel/CarouselEffect';
 import Category from './Components/Category/Category';
 import Header from './Components/Header/Header';
+import Product from './Components/Product/Product';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Header/>
         <CarouselEffect/>
         <Category/>
+        <Product/>
     </div>
   );
 }
