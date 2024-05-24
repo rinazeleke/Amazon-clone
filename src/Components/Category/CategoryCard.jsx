6df13a64477bbbceec080983b6a5084280d5779f
rinazeleke <rinazeleke1994@gmail.com>
2024-05-23 22:05:28 -1200

    Category rawting

--- src/Components/Category/CategoryCard.jsx
+++ src/Components/Category/CategoryCard.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import classes from "./Category.module.css";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function CategoryCard({ data }) {
   return (
     <div className={classes.category}>
-      {/* <Link to={`/category/${data.name}`}> */}
+      <Link to={`/category/${data.name}`}>
         <span>
           <h2>{data.title}</h2>
         </span>
         <img src={data.imgLink} alt={data.name} />
         <p>shop now</p>
-      {/* </Link> */}
+      </Link>
     </div>
   );
 }
