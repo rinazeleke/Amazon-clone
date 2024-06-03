d728d3c87225a65b6cc31d073a208a9cf8364c97
rinazeleke <rinazeleke1994@gmail.com>
2024-06-03 09:48:38 -1200

    diployment

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,8 @@
 import axios from "axios"
 
 const  axiosInstance = axios.create({
-    baseURL: "http://localhost:3000"
+    // baseURL: "http://localhost:3000"
+    baseURL:"https://amazon-api-deploy-2rqy.onrender.com/",
 });
 
 export {axiosInstance};
